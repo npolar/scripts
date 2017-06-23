2b92f6ebd01f50239d713275221686cff23f0004
srldl <siri.uldal@gmail.com>
2017-06-23 15:27:14 +0200

    final testing missing

--- radiation-weather/average60_3600.rb
+++ radiation-weather/average60_3600.rb
@@ -1,5 +1,5 @@
 #!/usr/bin/env ruby
-# Convert from 10min radiationWeather data into the 1 hourRadiation-weather database
+# Convert from 10min radiation-weather data into the 1 hourRadiation-weather database
 #
 # Author: srldl
 #
@@ -59,7 +59,7 @@ module Couch
     Dir.foreach("./data") {|x|
 
         #Throw away old array
-        @record_arr = []
+        @record_arr = Array.new(16, 0)
 
       unless ((x =~ /^\.\.?$/) && (!x.start_with? '2017')) #Drop . and .. dirs, start with 2017
 
@@ -67,38 +67,49 @@ module Couch
        CSV.foreach("./data/#{x}/1441_Npolar_Straaling10min.dat") do |row|
 
             @hour = 25
+            @at_2_avg =  @rh_2_avg = @ws_2_wvc1 = @ws_2_wvc2 = @sw_in_wpm2_avg = 0
+            @sw_out_wpm2_avg = @lw_in_corr_wpm2_avg = @lw_out_corr_wpm2_avg = 0
+            @row2 = nil
 
             #select all rows that starts with 20..
             if (row[0].match(/^20/))
 
-                #read the whole schema
-                row2 = CSV.read("./data/#{x}/1441_Npolar_Mast10min.dat")
-                puts row2.type
-                #Search a string for the right date
-                end
-                ....
+                @row2 = []
 
-                 #Get all 6 of them
-                if  row[0][11..12] == @hour
+                #read the whole schema
+                CSV.foreach("./data/#{x}/1441_Npolar_Straaling10min.dat") do |row3|
+                    if row3[1] ==row[1]
+                        @row2 = row3
+                    end
+                end
+
+
+                #Get all 6 of them
+                if  (row[0][11..12] == @hour ) || (@at_2_avg = @rh_2_avg = @ws_2_wvc1 = @ws_2_wvc2 = @sw_in_wpm2_avg = @sw_out_wpm2_avg = @lw_in_corr_wpm2_avg = @lw_out_corr_wpm2_avg == 0)
 
                     #Get the variables
-                    @at_2_avg = @at_2_avg + row2[5],
-                    @rh_2_avg = @rh_2_avg + row2[11]
-                    @ws_2_min = @ws_2_min +
-                    @gust_2_max = @gust_2_max + row2[21]
-                    @ws_2_wvc1 = @ws_2_wvc1 + row2[15]
-                    @ws_2_wvc2 = @ws_2_wvc2 + row2[16]
+                    @at_2_avg = @at_2_avg + @row2[5],
+                    @rh_2_avg = @rh_2_avg + @row2[11]
+                    #The min form 10 min data is not the same as the min from 60 min data which is samples every 30 sek! Thus, do not use these values!
+                   #  @ws_2_min = @ws_2_min +
+                  #  @gust_2_max = @gust_2_max + row2[21]
+                    @ws_2_wvc1 = @ws_2_wvc1 + @row2[15]
+                    @ws_2_wvc2 = @ws_2_wvc2 + @row2[16]
                     @sw_in_wpm2_avg = @sw_in_wpm2_avg + row[2]
                     @sw_out_wpm2_avg = @sw_out_wpm2_avg + row[4]
                     @lw_in_corr_wpm2_avg = @lw_in_corr_wpm2_avg + row[3]
                     @lw_out_corr_wpm2_avg = @lw_out_corr_wpm2_avg + row[5]
 
-                elsif @hour == 25
+                else
 
-                    u_east = mean(sin(WD * pi/180))
-                    u_north = mean(cos(WD * pi/180))
-                    avg_WD = arctan2(u_east, u_north) * 180/pi
-                    avg_WD = (360 + unit_WD) % 360
+                    #Update @hour to current hour of the day
+                    @hour = row[0][11..12]
+
+                    #Est the wind direction
+                    # u_east = mean(sin(WD * pi/180))
+                    # u_north = mean(cos(WD * pi/180))
+                    # avg_WD = arctan2(u_east, u_north) * 180/pi
+                    # avg_WD = (360 + unit_WD) % 360
 
                     #Get uuid
                     uuid = getUUID(server)
@@ -112,24 +123,26 @@ module Couch
                         :interval => '3600',
                         :timestamp => row[0][0..9] + 'T' + row[0][11..18] + 'Z', #trenger iso8601
                         :record => row[1],
-                        :at_2_avg => ,
-                        :rh_2_avg => ,
-                        :ws_2_min => ,
-                        :gust_2_max => ,
-                        :ws_2_wvc1 => ,
-                        :ws_2_wvc2 => ,
-                        :sw_in_wpm2_avg => ,
-                        :sw_out_wpm2_avg =>  ,
-                        :lw_in_corr_wpm2_avg => ,
-                        :lw_out_corr_wpm2_avg => ,
-                        :created => timestamp2,
-                        :updated => timestamp2,
+                        :at_2_avg => @at_2_avg,
+                        :rh_2_avg => @rh_2_avg,
+                        :ws_2_wvc1 => @ws_2_wvc1,
+                        :ws_2_wvc2 => @ws_2_wvc2,
+                        :sw_in_wpm2_avg => @sw_in_wpm2_avg,
+                        :sw_out_wpm2_avg => @sw_out_wpm2_avg,
+                        :lw_in_corr_wpm2_avg => @lw_in_corr_wpm2_avg,
+                        :lw_out_corr_wpm2_avg => @lw_out_corr_wpm2_avg,
+                        :created => timestamp,
+                        :updated => timestamp,
                         :created_by => user,
                         :updated_by => user
                     }
 
+                    @at_2_avg =  @rh_2_avg = @ws_2_wvc1 = @ws_2_wvc2 = @sw_in_wpm2_avg = nil
+                    @sw_out_wpm2_avg = @lw_in_corr_wpm2_avg = @lw_out_corr_wpm2_avg = nil
+
                     #remove nil values
                     @entry.reject! {|k,v| v.nil?}
+                    puts @entry
 
                     #Post entry
                   #  doc = @entry.to_json
