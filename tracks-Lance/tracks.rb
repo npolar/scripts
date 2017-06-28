d1bbf91da17bfa22ed8b6e10f35befe1c6746617
srldl <siri.uldal@gmail.com>
2017-06-28 15:12:40 +0200

    start reading files

--- tracks-Lance/tracks.rb
+++ tracks-Lance/tracks.rb
@@ -16,7 +16,6 @@ require 'date'
 require 'json'
 
 
-
 module Couch
 
   class Tracks
@@ -57,196 +56,106 @@ module Couch
     @entry = Hash.new
 
     Dir.foreach("./data") {|x|
-        puts x.to_s
-        puts "--------------------"
 
-        #Throw away old array
-        @record_arr = []
+        #If file .nav file
+        if x == "#{x[0..7]}nav.txt"
 
-      unless (x =~ /^\.\.?$/) #Drop . and .. dirs
+               #Extract info from nav file
+               previous = "0"
+               CSV.foreach("./data/#{x[0..7]}nav.txt") do |nav|
 
-        #There are two files Mast and Straaling
-       CSV.foreach("./data/#{x}") do |row|
-=begin
-            #select all rows that starts with 20..
-            if (row[0].match(/^20/))
+                  #Sample every min. Select all rows that starts with 00..
+                  if (nav[0].match(/^00/)) && (nav[0] != previous)
+                      previous = nav[0]
+                      puts nav[0]
+                  end
+               end
+
+               #Extract info from met file
+=begin              CSV.foreach("./data/#{x[0..7]}met.txt") do |met|
+                  #Find value closest to sampling time
+                  puts met.to_s + "  ccccc"
+               end
+
+               begin
+                    #Extract info from depth file
+                    CSV.foreach("./data/#{x[0..7]}depth.txt") do |depth|
+                           #Find value closest to sampling time
+                          puts depth.to_s + "  ccccc"
+                    end
+               ensure
+               end
+
+               begin
+                    #Extract info from wind file
+                    CSV.foreach("./data/#{x[0..7]}wind.txt") do |wind|
+                           #Find value closest to sampling time
+                          puts wind.to_s + "  ccccc"
+                    end
+                ensure
+                end
+=end
+
+               abort("ending")
 
             #Get uuid
             uuid = getUUID(server)
-           # puts row[1].to_s + "***"
+
+            #If date between 30.05 and 20.04 it is the "polar bear monitoring MOSJ" exped
+            if  (Date.parse(x[0..1]+"/"+x[2..3]+"/"+x[4..7])).between?(Date.parse("20/04/2017"),Date.parse("30/03/2017"))
+              @code = "polar bear monitoring MOSJ"
+              @expedition = "https://data.npolar.no/expedition/8b4b8b5c-7261-4abd-a56b-37cbbc078e38"
+            else #else "ICE-Whales"
+              @code = "ICE-Whales"
+              @expedition = "https://data.npolar.no/expedition/fcf8bad7-2b0a-47a7-bb97-9115d8855638"
+            end
+
 
             @entry = {
-                 :schema => 'http://api.npolar.no/schema/radiation-weather',
-                 :collection => 'radiation-weather',
                  :id => uuid,
                  :_id => uuid,
-                 :instrument_station => 'M KNG-6',
-                 :interval => '600',
-                 :timestamp => row[0][0..9] + 'T' + row[0][11..18] + 'Z', #trenger iso8601
-                 :record => row[1],
-                 :sw_in_wpm2_avg => row[2],
-                 :lw_in_corr_wpm2_avg => row[3],
-                 :sw_out_wpm2_avg => row[4],
-                 :lw_out_corr_wpm2_avg => row[5],
-                 :sw_in_t_c_avg => row[6],
-                 :lw_in_t_c_avg => row[7],
-                 :sw_out_t_c_avg => row[8],
-                 :lw_out_t_c_avg => row[9],
-                 :sw_in_fan_hz_avg => row[10],
-                 :lw_in_fan_hz_avg => row[11],
-                 :sw_out_fan_hz_avg => row[12],
-                 :lw_out_fan_hz_avg => row[13],
-                 :sw_in_wpm2_std => row[14],
-                 :lw_in_corr_wpm2_std => row[15],
-                 :sw_out_wpm2_std => row[16],
-                 :lw_out_corr_wpm2_std => row[17],
-                 :battv => nil,
-                 :ptemp_c => nil,
-                 :battbankv_avg => nil,
-                 :at_2_avg => nil,
-                 :at_4_avg => nil,
-                 :at_10_avg => nil,
-                 :apogeefan_avg1 => nil,
-                 :apogeefan_avg2 => nil,
-                 :apogeefan_avg3 => nil,
-                 :rh_2_avg => nil,
-                 :rh_4_avg => nil,
-                 :rh_10_avg => nil,
-                 :p_sfc_avg => nil,
-                 :ws_2_wvc1 => nil,
-                 :ws_2_wvc2 => nil,
-                 :ws_4_wvc1  => nil,
-                 :ws_4_wvc2 => nil,
-                 :ws_10_wvc1 => nil,
-                 :ws_10_wvc2 => nil,
-                 :gust_2_max => nil,
-                 :gust_4_max  => nil,
-                 :gust_10_max => nil,
-                 :tcdt => nil,
-                 :q  => nil,
+                 :air_pressure => '',
+                 :air_temperature => '',
+                 :code => @code,
+                 :collection => 'track',
+                 :course => '',
                  :created => timestamp,
-                 :updated => timestamp,
                  :created_by => user,
-                 :updated_by => user
+                 :depth => '',
+                 :expedition => @expedition,
+                 :humidity => '',
+                 :latitude => '',
+                 :longitude => '',
+                 :measured => 'time----',
+                 :object => "ship",
+                 :platform => "RV Lance",
+                 :sampling_rate => "",
+                 :schema => "http://api.npolar.no/schema/expedition_track",
+                 :sea_temperature => "",
+                 :speed => "",
+                 :updated => timestamp,
+                 :updated_by => user,
+                 :wind_direction_mean => "",
+                 :wind_speed_mean => ""
             }
 
+            #Get met.txt file
 
-       CSV.foreach("./data/#{x}/1441_Npolar_Mast10min.dat") do |row2|
+            #Get depth.txt file -if existing
 
+            #Get wind.txt - if existing
 
-
-       #if row2[1] is equal to
-       if (row[0].match(/^20/) && row[1] === row2[1])
-
-            # puts row2[1].to_s + "***2"
-
-             @entry[:battv] = row2[2]
-             @entry[:ptemp_c] = row2[3]
-             @entry[:battbankv_avg] = row2[4]
-             @entry[:at_2_avg] = row2[5]
-             @entry[:at_4_avg] = row2[6]
-             @entry[:at_10_avg] = row2[7]
-             @entry[:apogeefan_avg1] = row2[8]
-             @entry[:apogeefan_avg2] = row2[9]
-             @entry[:apogeefan_avg3] = row2[10]
-             @entry[:rh_2_avg] = row2[11]
-             @entry[:rh_4_avg] = row2[12]
-             @entry[:rh_10_avg] = row2[13]
-             @entry[:p_sfc_avg] = row2[14]
-             @entry[:ws_2_wvc1] = row2[15]
-             @entry[:ws_2_wvc2] = row2[16]
-             @entry[:ws_4_wvc1]  = row2[17]
-             @entry[:ws_4_wvc2] = row2[18]
-             @entry[:ws_10_wvc1] = row2[19]
-             @entry[:ws_10_wvc2] =  row2[20]
-             @entry[:gust_2_max] =  row2[21]
-             @entry[:gust_4_max]  = row2[22]
-             @entry[:gust_10_max] = row2[23]
-             @entry[:tcdt] = row2[24]
-             @entry[:q]  = row2[25]
-       end
-
-    end #CSV
-
-    #remove nil values
-    @entry.reject! {|k,v| v.nil?}
-
-    #Skip if empty!!
-    puts @entry[:record].to_s + @entry[:timestamp].to_s + " " + @entry[:id].to_s
-
-    @record_arr.push(@entry[:record])
-
-    #Post entry
-    doc = @entry.to_json
-
-    res2 = server.post("/weather-radiation/", doc, user, password)
-
-    end #if
-    end #CSV
-
-    #Need to scan through file 1441_Npolar_Mast10min.dat if there are entries that do not exist in
-    CSV.foreach("./data/#{x}/1441_Npolar_Mast10min.dat") do |row3|
-       #if (row3[1].match(/^20/)
-
-       #Compare file entries with array with entries from Straaling.
-       res = @record_arr.select { |e| e == row3[1]}
-       res.empty? {
-             #Get uuid
-            uuid = getUUID(server)
-            puts row3[1].to_s + "   ANY"
-
-            @entry = {
-                 :schema => 'http://api.npolar.no/schema/radiation-weather',
-                 :collection => 'radiation-weather',
-                 :id => uuid,
-                 :_id => uuid,
-                 :instrument_station => 'M KNG-6',
-                 :interval => '600',
-                 :timestamp => row3[0][0..9] + 'T' + row[0][11..18] + 'Z', #trenger iso8601
-                 :record => row3[1],
-                 :battv => row3[2],
-                 :ptemp_c => row3[3],
-                 :battbankv_avg => row3[4],
-                 :at_2_avg => row3[5],
-                 :at_4_avg => row3[6],
-                 :at_10_avg => row3[7],
-                 :apogeefan_avg1 => row3[8],
-                 :apogeefan_avg2 => row3[9],
-                 :apogeefan_avg3 => row3[10],
-                 :rh_2_avg => row3[11],
-                 :rh_4_avg => row3[12],
-                 :rh_10_avg => row3[13],
-                 :p_sfc_avg => row3[14],
-                 :ws_2_wvc1 => row3[15],
-                 :ws_2_wvc2 => row3[16],
-                 :ws_4_wvc1  => row3[17],
-                 :ws_4_wvc2 => row3[18],
-                 :ws_10_wvc1 => row3[19],
-                 :ws_10_wvc2 =>  row3[20],
-                 :gust_2_max =>  row3[21],
-                 :gust_4_max  => row3[22],
-                 :gust_10_max => row3[23],
-                 :tcdt => row3[24],
-                 :q  => row3[25],
-                 :created => timestamp,
-                 :updated => timestamp,
-                 :created_by => user,
-                 :updated_by => user
-            }
 
             #remove nil values
             @entry.reject! {|k,v| v.nil?}
 
             #Post entry
             doc = @entry.to_json
+            puts doc
 
-            res2 = server.post("/weather-radiation/", doc, user, password)
-       } #any
-=end
-    end #CSV
+           # res2 = server.post("/expedition_track/", doc, user, password)
 
-
-end #unless
+    end #if
 } #for each
 
 
