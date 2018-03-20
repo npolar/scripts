f518750062ef33ed63bac54e26a76899a98a3d14
srldl <siri.uldal@gmail.com>
2018-03-20 15:29:47 +0100

    frame, excotox doc

--- ecotox/prepareEcotox.js
+++ ecotox/prepareEcotox.js
@@ -1,6 +1,28 @@
+//js-xlsx
    XLSX = require('xlsx');
+   var fs = require('fs');
+   var filename = 'OC\ kylling\ blod\ 99.xls';
 
-   var workbook = XLSX.readFile('kylling.xls');
+
+  /*write to csv file */
+  function convertToCSV(objArray) {
+       var obj = typeof objArray != 'object' ? JSON.parse(objArray) : objArray;
+       var str = 'spreadsheet | id | date | lat | lng | expedition | sample_method \r\n';
+
+    //   for (var i = 0; i < Object.keys(obj).length; i++) {
+           var line = '';
+           for (var key in obj){
+             var attrName = key;
+             var attrValue = obj[key];
+              console.log(attrName, attrValue);
+              line += attrValue + "|";
+           }
+           str += line + '\r\n';
+    //   }
+       return str;
+  }
+
+   var workbook = XLSX.readFile(filename);
 
    var first_sheet_name = workbook.SheetNames[0];
    var address_of_cell = 'A1';
@@ -14,3 +36,16 @@
    /* Get the value */
    var desired_value = desired_cell.v;
    console.log(desired_value);
+
+   var entry = {species:"Rissa Tridactyla", matrix:"egg", individual:"K1"};
+
+   var out = convertToCSV(entry);
+
+
+fs.writeFile("./test", out, function(err) {
+    if(err) {
+        return console.log(err);
+    }
+
+    console.log("File created");
+});
